fd65c991ce8806acd08fae14a6b21b77f9400941
sarah <sarah@triggersandsparks.com>
2013-12-06 21:25:50 +0200

    Revise Samara's writeup and add links to full images.

--- pieces/samara-jensen-website/samara-jensen-website.php
+++ pieces/samara-jensen-website/samara-jensen-website.php
@@ -1,5 +1,5 @@
 <div class="twelvecol">
-	<img src="<?php echo $path; ?>/leader.png" alt="Samara Jensen: Author Site" />
+	<a href="<?php echo $path; ?>/leader.png"><img src="<?php echo $path; ?>/leader.png" alt="Samara Jensen: Author Site" /></a>
 </div>
 
 
@@ -8,13 +8,13 @@
 <div class="row">
 
 	<div class="sixcol">
-		<img src="<?php echo $path; ?>/detail.png" alt="Detail" />
+		<a href="<?php echo $path; ?>/detail.png"><img src="<?php echo $path; ?>/detail.png" alt="Detail" /></a>
 	</div>
 		
 	<div class="sixcol last project-writeup">
 		<p class="cap">Samara is a new young adult author. Her books are full of adventure and fantasy, and she wanted a website design that would reflect that in a dreamy, stylish way. On the technical side, she wanted a site that would be as flexible and easy-to-update as possible.</p>
 
-		<p>I worked with Samara to design a theme that incorporated a few customized stock illustrations to give a whimsical fairy-tale feel. The girl reading a book on the moon in the header is repeated in the footer as a mermaid to lend a feeling of continuity between sky and water, and the typography was chosen carefully to feel both friendly and stylish. We updated the whole website to run as a customized Wordpress installation, then created some custom graphics for various uses, including Twitter and Facebook.</p>
+		<p>I worked with Samara to design a WordPress theme, using customized stock illustrations to give a whimsical, fairy-tale feel. The girl at the top of the page, reading on the moon, is mirrored in the footer as a mermaid to lend a feeling of continuity between sky and water. Typography was chosen carefully to feel at once both friendly and stylish.</p>
 	</div>
 
 </div>
@@ -25,15 +25,15 @@
 <div class="row">
 
 	<div class="fourcol">
-		<img src="<?php echo $path; ?>/mermaid.png" alt="Mermaid" />
+		<a href="<?php echo $path; ?>/mermaid.png"><img src="<?php echo $path; ?>/mermaid.png" alt="Mermaid" /></a>
 	</div>
 
 	<div class="fourcol">
-		<img src="<?php echo $path; ?>/reader.png" alt="Girl reading" />
+		<a href="<?php echo $path; ?>/reader.png"><img src="<?php echo $path; ?>/reader.png" alt="Girl reading" /></a>
 	</div>
 	
 	<div class="fourcol">
-		<img src="<?php echo $path; ?>/ship.png" alt="Ship" />
+		<a href="<?php echo $path; ?>/ship.png"><img src="<?php echo $path; ?>/ship.png" alt="Ship" /></a>
 	</div>
 
 </div>
@@ -48,18 +48,18 @@
 		<p>As a new author on the block, I needed a website that would help my work stand out from the crowd and allow me to build an online platform. Sarah gave me all that and more, taking a rough design concept and a very long wish-list and turning it into a reality. She provided help and support every step of the way, with a two-way design process that took all of my feedback into account, and quickly answered all of my technical questions. I'm thrilled with my new website and I'm so glad I decided to work with Sarah!</p>
 		</blockquote>
 
-		<img src="<?php echo $path; ?>/long-shot.png" alt="Scrolling page" />
+		<a href="<?php echo $path; ?>/long-shot.png"><img src="<?php echo $path; ?>/long-shot.png" alt="Scrolling page" /></a>
 	</div>
 
 	<div class="sixcol last">
-		<img src="<?php echo $path; ?>/contact.png" alt="Contact page" />
+		<a href="<?php echo $path; ?>/contact.png"><img src="<?php echo $path; ?>/contact.png" alt="Contact page" /></a>
 
 		<span class="date url-link">&middot; <a href="http://samarajensen.com">samarajensen.com</a> &middot; </span>
 
 		<p>A matching Facebook header graphic and a background for Twitter rounded out the package, along with some buttons and badges of various sizes for Samara to use around the internet.</p>
 
-		<img src="<?php echo $path; ?>/facebook2.png" alt="Facebook" />
-		<img src="<?php echo $path; ?>/twitter.png" alt="Twitter" />
+		<a href="<?php echo $path; ?>/facebook2.png"><img src="<?php echo $path; ?>/facebook2.png" alt="Facebook" /></a>
+		<a href="<?php echo $path; ?>/twitter.png"><img src="<?php echo $path; ?>/twitter.png" alt="Twitter" /></a>
 
 	</div>
 
