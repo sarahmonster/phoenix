f2765824c71da26bd02a171235ec604e01ad6dcc
sarah <sarah@triggersandsparks.com>
2013-12-06 17:01:25 +0200

    Set up basic page for Samara Jensen site.

---
+++ pieces/samara-jensen-website/samara-jensen-website.php
@@ -0,0 +1,66 @@
+<div class="twelvecol">
+	<img src="<?php echo $path; ?>/leader.png" alt="Samara Jensen: Author Site" />
+</div>
+
+
+
+
+<div class="row">
+
+	<div class="sixcol">
+		<img src="<?php echo $path; ?>/detail.png" alt="Detail" />
+	</div>
+		
+	<div class="sixcol last project-writeup">
+		<p class="cap">Samara is a new young adult author. Her books are full of adventure and fantasy, and she wanted a website design that would reflect that in a dreamy, stylish way. On the technical side, she wanted a site that would be as flexible and easy-to-update as possible.</p>
+
+		<p>I worked with Samara to design a theme that incorporated a few customized stock illustrations to give a whimsical fairy-tale feel. The girl reading a book on the moon in the header is repeated in the footer as a mermaid to lend a feeling of continuity between sky and water, and the typography was chosen carefully to feel both friendly and stylish. We updated the whole website to run as a customized Wordpress installation, then created some custom graphics for various uses, including Twitter and Facebook.</p>
+	</div>
+
+</div>
+
+
+
+
+<div class="row">
+
+	<div class="fourcol">
+		<img src="<?php echo $path; ?>/mermaid.png" alt="Mermaid" />
+	</div>
+
+	<div class="fourcol">
+		<img src="<?php echo $path; ?>/reader.png" alt="Girl reading" />
+	</div>
+	
+	<div class="fourcol">
+		<img src="<?php echo $path; ?>/ship.png" alt="Ship" />
+	</div>
+
+</div>
+
+
+
+
+<div class="row">
+
+	<div class="sixcol quote">
+		<blockquote>
+		<p>As a new author on the block, I needed a website that would help my work stand out from the crowd and allow me to build an online platform. Sarah gave me all that and more, taking a rough design concept and a very long wish-list and turning it into a reality. She provided help and support every step of the way, with a two-way design process that took all of my feedback into account, and quickly answered all of my technical questions. I'm thrilled with my new website and I'm so glad I decided to work with Sarah!</p>
+		</blockquote>
+
+		<img src="<?php echo $path; ?>/long-shot.png" alt="Scrolling page" />
+	</div>
+
+	<div class="sixcol last">
+		<img src="<?php echo $path; ?>/contact.png" alt="Contact page" />
+
+		<span class="date url-link">&middot; <a href="http://samarajensen.com">samarajensen.com</a> &middot; </span>
+
+		<p>A matching Facebook header graphic and a background for Twitter rounded out the package, along with some buttons and badges of various sizes for Samara to use around the internet.</p>
+
+		<img src="<?php echo $path; ?>/facebook2.png" alt="Facebook" />
+		<img src="<?php echo $path; ?>/twitter.png" alt="Twitter" />
+
+	</div>
+
+</div>
