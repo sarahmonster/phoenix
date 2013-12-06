f1be161930eea9eba1e14f8b172847b0e976bfc6
sarah <sarah@triggersandsparks.com>
2013-12-06 18:59:09 +0200

    Add basic page setup for Paul Leone.

---
+++ pieces/paul-leone-author-website/paul-leone-author-website.php
@@ -0,0 +1,58 @@
+<div class="twelvecol">
+	<img class="borderless" src="<?php echo $path; ?>/responsive.png" alt="Paul Leone: Responsive Author Site" />
+</div>
+
+
+
+
+<div class="row">
+
+	<div class="sixcol">
+		<img src="<?php echo $path; ?>/blog-post.jpg" alt="Blog post" />
+	</div>
+		
+	<div class="sixcol last project-writeup">
+		<p class="cap">Paul came to me when he was looking to publicize his new self-published series, a new urban fantasy series about Catholic vampire hunters. Appreciating a new spin on the genre, (Dear everyone: less <em>Twilight</em>, please!) I was more than happy to help him.</p>
+
+		<p>He&rsquo;d already had the cover for his first title designed, and wanted the site to fit. I used the source imagery, rebuilt it a bit, and worked some of the imagery into the background of the page. </p>
+		
+		<span class="date url-link">&middot; <a href="http://paul-leone.com">paul-leone.com</a> &middot; </span>
+		
+		<p>I decided to run a bit with the Catholic vampires theme, and added icons to the menu that had a distinctly Catholic flair. Handy that that just naturally seemed to fit with the vampire theme, as well!</p>
+	</div>
+
+</div>
+
+
+
+
+<div class="row">
+
+	<div class="fourcol">
+		<img src="<?php echo $path; ?>/mobile-menu.png" alt="Menu on mobiles" />
+	</div>
+
+	<div class="eightcol">
+		<img src="<?php echo $path; ?>/welcome-screen.jpg" alt="Homepage on desktop" />
+	</div>
+
+</div>
+
+
+
+
+<div class="row">
+	
+	<div class="sixcol">
+	
+	Image
+		
+	</div>
+	
+	<div class="sixcol quote">
+		<blockquote>
+		<p>Sarah was a pleasure to work with. She was friendly and explained each stage of the web-​​design process as it unfolded (no easy task for an oft-​​befuddled history major), and the end result was everything I could have hoped for. Thoroughly professional work and someone I wouldn’t hesitate to recommend for those looking for a quality website.</p>
+		</blockquote>
+	</div>
+
+</div>
