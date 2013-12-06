423abe6a54e0fc1c4629888f5ed1bc69a934aede
sarah <sarah@triggersandsparks.com>
2013-12-06 19:53:05 +0200

    Rewrite Denise's page a bit and increase spacing of project writeups.

--- pieces/denise-doucet-inc-website/denise-doucet-inc-website.php
+++ pieces/denise-doucet-inc-website/denise-doucet-inc-website.php
@@ -11,11 +11,11 @@
 	</div>
 	
 	<div class="sixcol project-writeup">
-		<p class="cap">Denise needed a simple, brochure-style site that would serve to advertise and inform about her brand-new government relations firm. Her request was for something elegant, simple, and clean that could be expanded to feature more content and functionality in the future. </p>
+		<p class="cap">Denise came to me looking for a simple, brochure-style site that would serve to advertise and inform about her brand-new government relations firm. Her request was for something elegant, simple, and clean that could be expanded to feature more content and functionality in the future. </p>
 	
 		<span class="date url-link">&middot; <a href="http://denisedoucetinc.com">denisedoucetinc.com</a> &middot; </span>
 	
-		<p>Naturally, I built her a WordPress site. While the site doesn't currently make use of WordPress' blogging functionality, this could easily be activated. When I suggested that we make it responsive, she was immediately amenable to the idea. The end result is a site that's simple, clean, and professional, while still letting Denise's personality shine through.</p>
+		<p>We started by building her a custom WordPress site, careful to keep the structure as flexible as possible, in anticipation of future expansion. I suggested we opt for a responsive design, especially since many of her clients would be browsing their site on their phones. The end result is a site that's simple, clean, and professional, while still letting Denise's personality shine through.</p>
 	
 		<a href="<?php echo $path; ?>/logo.png"><img src="<?php echo $path; ?>/logo.png" alt="Denise Doucet INC logo" /></a>
 	
@@ -32,8 +32,8 @@
 		<a href="<?php echo $path; ?>/circularlogo.png"><img class="borderless" src="<?php echo $path; ?>/circularlogo.png" alt="Circular logo" /></a>
 	</div>
 	
-	<div class="nineecol" style="padding-top: 80px;">
-		<p>In the process of designing the site, we'd developed a simple, but strong, logotype. I provided Denise with a few variants of this to use across different media. Then I expanded it a bit, and created a circular variant that would be more appropriate for certain situations (Facebook page, etc).</p> 
+	<div class="nineecol" style="padding-top: 40px;">
+		<p>In the process of designing the site, we&rsquo;d developed a simple&mdash;but strong&mdash;logotype. I provided Denise with a few variants of this to use across different media. Since the logo is quite rectangular, I also created a circular variant that would be more appropriate for certain situations. This variant works well as an icon, or for a Facebook page, or anywhere where you need a logo to be more square in proportion, giving Denise more flexibility with her branding.</p> 
 	</div>
 
 </div>
@@ -50,7 +50,7 @@
 	
 	<div class="sixcol last project-writeup">
 		
-		<p class="cap">Since the project seemed largely straightforward, I took advantage of the opportunity to learn a few new tricks—prinarily, employing a new WordPress starter theme and finally learning SASS. I use both now as a default for all new WordPress sites.</p> 
+		<p class="cap">Since the project seemed largely straightforward, I took advantage of the opportunity to learn a few new tricks. I used a new WordPress starter theme, after careful evaluation of the options available. While I was at it, I finally taught myself to use SASS, which has improved my development practises immensely. I now use both as a default for all new WordPress sites.</p> 
 	</div>
 
 </div>
