064e2f32e9d097561e8654d06d50ecfa819f787b
sarah <sarah@triggersandsparks.com>
2013-12-07 16:30:43 +0200

    Expand on main text for EL's page.

--- pieces/el-tettensor-author-website/el-tettensor-author-website.php
+++ pieces/el-tettensor-author-website/el-tettensor-author-website.php
@@ -11,18 +11,39 @@
 	</div>
 	
 	<div class="fivecol project-writeup">
-		<p class="cap">When E.L. Tettensor asked me to build her a website, she told me she liked Dan Brown&rsquo;s site, which made use of a one-page scrolling parallax effect, but that it was a bit out of reach, as a new author. Naturally, I took this as a challenge, and decided that I could learn, not just how to make a parallax WordPress site, but that I could also make it responsive.</p>
+		<p class="cap">When E.L. Tettensor asked me to build her a website to promote her debut novel, she said she wanted something that felt like an immersive experience&mdash;like stepping inside the pages of the book. She also mentioned that she liked the parallax scrolling effect used on Dan Brown&rsquo;s site, but that it might be a bit out of reach. Naturally, I took this as a challenge. This site was my first experience with parallax effects&mdash;and of course I decided that it needed to be responsive, as well.</p>
 		
-		<p>The next few weeks indicated that I might have over-estimated my own abilities.</p>
+		<p>A few sleepless nights prior to our launch, I realized that I might have over-estimated my own abilities.</p>
 		
 		<a href="<?php echo $path; ?>/logo.png"><img class="borderless" src="<?php echo $path; ?>/logo.png" alt="" /></a>
 	
+		<p>First, I helped E.L. find a great illustrator for the main background image. Once that was done, I built the design of the site around the illustration. The final design is something of a collage. I built backgrounds for the different panels by combining elements extracted from the illustration, from my own libraries, and from various stock sources, modifying and combining them so that each panel would feel distinct, but that they belonged to a greater whole. In order to make the transition from panel to panel less abrupt, I added an extra panel in between with subtle text and imagery that scrolled at different speeds, creating an illusion of depth and a bit more dynamism to the page. It's a rather image-heavy site, but the mixing of all these patterns, textures, and ornaments makes the site feel appropriately rich.</p>
+		
+		<p>I also did some CSS animation on the front page, to make it feel more as though the visitor is walking in on a scene. The background image fades in quickly over an out-of-focus, desaturated, and almost black version of itself, giving the effect of a light being switched on. Then the logo, navigation, text, and buttons fade in at different speeds, while a wisp of green smoke fades in almost imperceptibly.</p>
+		
+		
 		<span class="date url-link">&middot; <a href="http://eltettensor.com">eltettensor.com</a> &middot; </span>
 	
-		<a href="<?php echo $path; ?>/blog-post.jpg"><img src="<?php echo $path; ?>/blog-post.jpg" alt="" /></a>
-
 	</div>
 
 </div>
 
 
+<div class="row">
+
+	<div class="fourcol">
+		<p>The blog posts are separate pages, and have their own style that ties in with the rest of the site. </p>
+		
+		<p>Something about mobile!</p>
+	</div>
+
+	<div class="fourcol">
+		<a href="<?php echo $path; ?>/blog-post.jpg"><img src="<?php echo $path; ?>/blog-post.jpg" alt="" /></a>
+	</div>
+	
+	<div class="fourcol">
+		<a href="<?php echo $path; ?>/blog-post.jpg"><img src="<?php echo $path; ?>/blog-post.jpg" alt="" /></a>
+	</div>
+	
+
+</div>
