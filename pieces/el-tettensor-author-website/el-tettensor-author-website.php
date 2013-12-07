274985abaa29367c6f27f0ee8a0d4d15dd3c901d
sarah <sarah@triggersandsparks.com>
2013-12-07 18:59:10 +0200

    Add phone/tablet images and refine text a bit more.

--- pieces/el-tettensor-author-website/el-tettensor-author-website.php
+++ pieces/el-tettensor-author-website/el-tettensor-author-website.php
@@ -4,6 +4,9 @@
 
 
 
+
+
+
 <div class="row">
 
 	<div class="sevencol">
@@ -11,17 +14,16 @@
 	</div>
 	
 	<div class="fivecol project-writeup">
-		<p class="cap">When E.L. Tettensor asked me to build her a website to promote her debut novel, she said she wanted something that felt like an immersive experience&mdash;like stepping inside the pages of the book. She also mentioned that she liked the parallax scrolling effect used on Dan Brown&rsquo;s site, but that it might be a bit out of reach. Naturally, I took this as a challenge. This site was my first experience with parallax effects&mdash;and of course I decided that it needed to be responsive, as well.</p>
+		<p class="cap">When E.L. Tettensor asked me to build her a website to promote her debut novel, she said she wanted an immersive experience&mdash;like stepping inside the pages of the book. It needed to be old-fashioned, dark, and ornate. She also mentioned that she liked the parallax scrolling effect used on Dan Brown&rsquo;s site, but that it might be a bit out of reach. Naturally, I took this as a challenge. This site was my first experience with parallax effects&mdash;and of course I decided that it needed to be responsive, as well.</p>
 		
 		<p>A few sleepless nights prior to our launch, I realized that I might have over-estimated my own abilities.</p>
 		
 		<a href="<?php echo $path; ?>/logo.png"><img class="borderless" src="<?php echo $path; ?>/logo.png" alt="" /></a>
 	
-		<p>First, I helped E.L. find a great illustrator for the main background image. Once that was done, I built the design of the site around the illustration. The final design is something of a collage. I built backgrounds for the different panels by combining elements extracted from the illustration, from my own libraries, and from various stock sources, modifying and combining them so that each panel would feel distinct, but that they belonged to a greater whole. In order to make the transition from panel to panel less abrupt, I added an extra panel in between with subtle text and imagery that scrolled at different speeds, creating an illusion of depth and a bit more dynamism to the page. It's a rather image-heavy site, but the mixing of all these patterns, textures, and ornaments makes the site feel appropriately rich.</p>
+		<p>First, I helped E.L. find a great illustrator for the main background image. I then designed the site around the illustration. The final design is something of a collage. I built backgrounds for the different panels by combining elements extracted from the illustration, from my own libraries, and from various stock sources, modifying and combining them so that each panel would feel distinct, but that they belonged to a greater whole. In order to make the transition from panel to panel less abrupt, I added an extra panel in between with subtle text and imagery that scrolled at different speeds, creating an illusion of depth. It's a rather image-heavy site, but the mixing of all these patterns, textures, and ornaments makes the site feel appropriately rich.</p>
 		
 		<p>I also did some CSS animation on the front page, to make it feel more as though the visitor is walking in on a scene. The background image fades in quickly over an out-of-focus, desaturated, and almost black version of itself, giving the effect of a light being switched on. Then the logo, navigation, text, and buttons fade in at different speeds, while a wisp of green smoke fades in almost imperceptibly.</p>
-		
-		
+				
 		<span class="date url-link">&middot; <a href="http://eltettensor.com">eltettensor.com</a> &middot; </span>
 	
 	</div>
@@ -29,21 +31,45 @@
 </div>
 
 
+
+
+
+
 <div class="row">
 
-	<div class="fourcol">
+	<div class="fourcol project-writeup">
+		<p class="cap">Making the site work well across a variety of screens, with different sizes, proportions, and technical restrictions, was a major challenge. A lot of work went into fine-tuning all the rules such that the site would work well everywhere.</p>
+		
 		<p>The blog posts are separate pages, and have their own style that ties in with the rest of the site. </p>
 		
-		<p>Something about mobile!</p>
 	</div>
 
 	<div class="fourcol">
-		<a href="<?php echo $path; ?>/blog-post.jpg"><img src="<?php echo $path; ?>/blog-post.jpg" alt="" /></a>
+		<a href="<?php echo $path; ?>/iphone.jpg"><img src="<?php echo $path; ?>/iphone.jpg" alt="iPhone" /></a>
 	</div>
 	
 	<div class="fourcol">
-		<a href="<?php echo $path; ?>/blog-post.jpg"><img src="<?php echo $path; ?>/blog-post.jpg" alt="" /></a>
+		<a href="<?php echo $path; ?>/ipad.jpg"><img src="<?php echo $path; ?>/ipad.jpg" alt="iPad" /></a>
 	</div>
 	
 
+</div>
+
+
+
+
+
+
+<div class="row">
+
+	<div class="fivecol">
+		<a href="<?php echo $path; ?>/blog-post.jpg"><img src="<?php echo $path; ?>/blog-post.jpg" alt="" /></a>
+	</div>
+	
+	<div class="sevencol">
+		<blockquote>
+			<p> </p>
+		</blockquote>
+	</div>
+	
 </div>
