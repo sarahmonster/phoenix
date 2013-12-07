c0fe3390725c8460e760a2d507c5bba9d1b6cafb
sarah <sarah@triggersandsparks.com>
2013-12-07 02:27:00 +0200

    Add initial template for EL Tettensor.

---
+++ pieces/el-tettensor-author-website/el-tettensor-author-website.php
@@ -0,0 +1,26 @@
+<div class="twelvecol">
+	<a href="<?php echo $path; ?>/responsive.png"><img class="borderless" src="<?php echo $path; ?>/responsive.png" alt="" /></a>
+</div>
+
+
+
+<div class="row">
+
+	<div class="fivecol">
+		<a href="<?php echo $path; ?>/parallax.jpg"><img src="<?php echo $path; ?>/parallax.jpg" alt="" /></a>
+	</div>
+	
+	<div class="sevencol project-writeup">
+		<p class="cap">When E.L. Tettensor asked me to build her a website, she told me she liked Dan Brown&rsquo;s site, which made use of a one-page scrolling parallax effect, but that it was a bit out of reach, as a new author. Naturally, I took this as a challenge, and decided that I could learn, not just how to make a parallax WordPress site, but that I could also make it responsive.</p>
+		
+		<p>The next few weeks indicated that I might have over-estimated my own abilities.</p>
+	
+		<span class="date url-link">&middot; <a href="http://eltettensor.com">eltettensor.com</a> &middot; </span>
+	
+		<a href="<?php echo $path; ?>/blog-post.jpg"><img src="<?php echo $path; ?>/blog-post.jpg" alt="" /></a>
+
+	</div>
+
+</div>
+
+
