fef511d2161f8554dfa24eb27a5d8e69156c650d
sarah <sarah@triggersandsparks.com>
2013-12-09 18:29:28 +0200

    Set up initial page for Cascades Spa.

---
+++ pieces/cascades-day-spa-website/cascades-day-spa-website.php
@@ -0,0 +1,73 @@
+<div class="twelvecol">
+	<a href="<?php echo $path; ?>/responsive.png"><img class="borderless" src="<?php echo $path; ?>/responsive.png" alt="" /></a>
+</div>
+
+
+
+
+
+
+<div class="row">
+
+	<div class="sevencol">
+		<a href="<?php echo $path; ?>/parallax.jpg"><img src="<?php echo $path; ?>/parallax.jpg" alt="" /></a>
+	</div>
+	
+	<div class="fivecol project-writeup">
+		<p class="cap">When E.L. Tettensor asked me to build her a website to promote her debut novel, she said she wanted an immersive experience&mdash;like stepping inside the pages of the book. It needed to be old-fashioned, dark, and ornate. She mentioned that she liked the parallax scrolling effect, but that it might be a bit out of reach. Naturally, I took this as a challenge. This site was my first experience with parallax effects—but I went a step further and made sure the site was responsive. The result is a site that&rsquo;s as immersive on a phone as on a computer.</p>
+				
+		<a href="<?php echo $path; ?>/logo.png"><img class="borderless" src="<?php echo $path; ?>/logo.png" alt="" /></a>
+	
+		<p>First, I helped E.L. find a great illustrator for the main background image. I then designed the site around the illustration. The final design is something of a collage. I built backgrounds for the different panels by combining elements extracted from the illustration, from my own libraries, and from various stock sources, modifying and combining them so that each panel would feel distinct, while remaining a cohesive whole. In order to make the transition from panel to panel less abrupt, I added an extra panel in between with subtle text and imagery that scrolled at different speeds, creating an illusion of depth. It's a rather image-heavy site, but the mixing of all these patterns, textures, and ornaments makes the site feel appropriately rich.</p>
+		
+		<p>I also did some CSS animation on the front page, to make it feel more as though the visitor is walking in on a scene. The background image fades in quickly over an out-of-focus, desaturated, and almost black version of itself, giving the effect of a light being switched on. Then the logo, navigation, text, and buttons fade in at different speeds, while a wisp of green smoke fades in almost imperceptibly.</p>
+				
+		<span class="date url-link">&middot; <a href="http://eltettensor.com">eltettensor.com</a> &middot; </span>
+	
+	</div>
+
+</div>
+
+
+
+
+
+
+<div class="row">
+
+	<div class="fourcol project-writeup">
+		<p class="cap">Making the site work well across a variety of screens, with different sizes, proportions, and technical restrictions, was a major challenge. A lot of work went into fine-tuning all the rules such that the site would work well everywhere.</p>
+		
+		<p>Part of the challenge of a one-page site like this lies in deciding what to do with content that won't fit into easy panels. As a result, I decided to structure the blog posts and archive as separate pages. These pages are styled to tie in with the rest of the site, and the header links back to the appropriate sections of the main page.</p>
+		
+	</div>
+
+	
+	<div class="fivecol">
+		<a href="<?php echo $path; ?>/ipad.jpg"><img src="<?php echo $path; ?>/ipad.jpg" alt="iPad" /></a>
+	</div>
+
+	<div class="threecol iphone">
+		<a href="<?php echo $path; ?>/iphone.jpg"><img src="<?php echo $path; ?>/iphone.jpg" alt="iPhone" /></a>
+	</div>
+
+</div>
+
+
+
+
+
+
+<div class="row">
+
+	<div class="fivecol">
+		<a href="<?php echo $path; ?>/blog-post.jpg"><img src="<?php echo $path; ?>/blog-post.jpg" alt="" /></a>
+	</div>
+	
+	<div class="sevencol">
+		<blockquote>
+			<p>I had a few good proposals for my website, but Sarah was absolutely the right choice. Not only did she completely understand what I was going for, she was able to execute to perfection, pairing an artist&rsquo;s eye with an engineer&rsquo;s attention to detail. After helping me find just the right artist to do the background illustration, she meticulously sourced the fonts and images, culminating in an organic, immersive experience that perfectly captures the mood of the books. Small wonder the website has received so many compliments! I fully expect to work with Sarah again in the future.</p>
+		</blockquote>
+	</div>
+	
+</div>
