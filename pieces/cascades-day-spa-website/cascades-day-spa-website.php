5bfcc7e87c591f3e60a6514cb539d9b9b39539ed
sarah <sarah@triggersandsparks.com>
2013-12-10 22:53:16 +0200

    Revise wording of Cascades page.

--- pieces/cascades-day-spa-website/cascades-day-spa-website.php
+++ pieces/cascades-day-spa-website/cascades-day-spa-website.php
@@ -11,14 +11,14 @@
 
 	
 	<div class="sixcol project-writeup">
-		<p class="cap">Cascades Day Spa is one of Halifax&rsquo;s longest-running and most well-known day spas. They have a loyal clientele and an excellent reputation, but their old website just wasn't pulling its weight anymore. Built on a Contribute platform, it was difficult to update, hard to navigate, and had a design that was severely out-of-date. They approached me knowing the site needed an overhaul in order to have a website that was in line with their business, and that would allow for continued growth.</p>
-				
-	<span class="date url-link">&middot; <a href="http://cascadesspa.com">cascadesspa.com</a> &middot; </span>
+		<p class="cap">Cascades Day Spa is one of Halifax&rsquo;s longest-running and most well-known day spas. They have a loyal clientele and an excellent reputation, but their old website just wasn&rsquo;t pulling its weight anymore. Built on a Contribute platform, it was difficult to update, hard to navigate, and its design was sorely outdated. The site needed an overhaul to be in line with their business and to allow for continued growth and new customers.</p>
+		
+	<p>They requested that the site be simple, clean, and easy to navigate. At the same time, they knew they needed something that would be accessible on phones and tablets. Of primary importance were the gift certificates and the book online functionality, both of which needed to be as easy to use as possible from both a client and a company perspective.</p>
+					
+	<p>After evaluating their precise needs from an administrative point of view, I suggested a few options for online gift certificates, set up an online appointment booking form, and set up a custom WordPress installation. Promotions are managed as a special post type, allowing for easy entry without replicating content in multiple places.</p>
 	
-	<p>After evaluating her precise needs from an administrative point of view, I suggested a few options for online gift certificates, set up a simple form for booking an appointment, and created a custom WordPress installation. Promotions are managed as a special post type, allowing for easy entry without replicating the same content.</p>
-	
-	<p>Simple isn't always as easy as it seems. A particular challenge inherent in this site was developing style rules that would work across a variety of different pages. Many of the pages are structured as price lists, and I played around with the rules quite a few times before settling on a set of rules that worked consistently and displayed well across all pages. The resulting lists are both aesthetically appealing and easily scanned.</p>
-	
+	<span class="date url-link">&middot; <a href="http://cascades-spa.com">cascades-spa.com</a> &middot; </span>
+		
 	</div>
 	
 	
@@ -43,8 +43,12 @@
 		<a href="<?php echo $path; ?>/ipad.jpg"><img src="<?php echo $path; ?>/ipad.jpg" alt="iPad" /></a>
 	</div>
 	
+
 	<div class="fourcol project-writeup">
-		<p class="cap">Kim requested that the site be simple, clean, and easy to navigate. At the same time, she knew that she needed something that would be accessible on phones and tablets. Of primary importance were the gift certificates and the book online functionality, both of which needed to be as easy to use as possible from both a client and a company perspective.</p>
+		
+		<p class="cap">Simple isn&rsquo;t always as easy as it seems. A particular challenge of this site was developing style rules appropriate to a variety of different pages. The prices and packages demanded a strong information hierarchy to be both aesthetically appealing and easily scannable. I adjusted the rules bit-by-bit, testing pages simultaneously, before settling on a set of styles that worked consistently across all pages.</p>
+		
+		<p>The site is built on a responsive framework, with customized menu and page structures for different devices. The result is a site as easy to access on the go as it is from home.</p>
 	</div>
 
 </div>
