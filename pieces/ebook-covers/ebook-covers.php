00321f76bed8c4ff283042552f152a856bedd1e6
sarah <sarah@triggersandsparks.com>
2013-12-10 21:51:10 +0200

    Set up inital book covers page.

---
+++ pieces/ebook-covers/ebook-covers.php
@@ -0,0 +1,12 @@
+<div class="twelvecol">
+	<a href="<?php echo $path; ?>/book-covers.png"><img class="borderless" src="<?php echo $path; ?>/book-covers.png" alt="" /></a>
+</div>
+
+
+<div class="row">
+
+	<div class="sixcol">
+		<p class="cap">Designing a book cover is basically my absolute dream job, so when Awful Agent approached me about doing some covers for ebooks they were putting out, I was of course delighted! It&rsquo;s not quite as glamorous as a real-live physical book cover, but it&rsquo;s close. Designing an ebook cover presents its own unique challenges.</p>
+	</div>
+
+</div>
