e755a05db78607b2a6023c3d1c9cbeef8d1333ab
sarah <sarah@triggersandsparks.com>
2013-12-10 21:58:49 +0200

    Add Lord Darcy series images and write-up.

--- pieces/ebook-covers/ebook-covers.php
+++ pieces/ebook-covers/ebook-covers.php
@@ -7,6 +7,33 @@
 
 	<div class="sixcol">
 		<p class="cap">Designing a book cover is basically my absolute dream job, so when Awful Agent approached me about doing some covers for ebooks they were putting out, I was of course delighted! It&rsquo;s not quite as glamorous as a real-live physical book cover, but it&rsquo;s close. Designing an ebook cover presents its own unique challenges.</p>
+		
+		<p>My first assignment was a series of three covers, for Randall Garret&rsquo;s much-loved Lord Darcy series, a fantasy trilogy set in an alternate version of Victorian England. They were looking for simple, striking covers that would fit together well. I developed a simple template, then used colour and imagery to differentiate the titles.</p>
+		
+		
 	</div>
 
+	<div class="sixcol">
+	
+	</div>
+
+</div>
+
+
+
+
+<div class="row">
+
+	<div class="fourcol">
+		<a href="<?php echo $path; ?>/Lord-Darcy-Investigates.jpg"><img class="borderless" src="<?php echo $path; ?>/Lord-Darcy-Investigates.jpg" alt="" /></a>
+	</div>
+
+	<div class="fourcol">
+		<a href="<?php echo $path; ?>/Murder-and-Magic.jpg"><img class="borderless" src="<?php echo $path; ?>/Murder-and-Magic.jpg" alt="" /></a>
+	</div>
+	
+	<div class="fourcol">
+		<a href="<?php echo $path; ?>/Too-Many-Magicians.jpg"><img class="borderless" src="<?php echo $path; ?>/Too-Many-Magicians.jpg" alt="" /></a>
+	</div>
+	
 </div>
