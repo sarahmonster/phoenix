ef78d5c36d47b9292008b21f2c73434075aeddeb
sarah <sarah@triggersandsparks.com>
2013-12-11 04:35:19 +0200

    Show client name rather than full title, if available.

--- pieces.php
+++ pieces.php
@@ -18,7 +18,18 @@
 		<img class="hover" src="<?php echo $thumbnail_path; ?>thumbnail-alt.jpg" />
 		
 		<div class="overlay">
-			<h2><?php echo the_title();  ?></h2>
+		<?php 
+		$client = get_post_meta($post->ID, 'Client', $single);
+		//echo $client[0];
+		
+		if ($client[0] != "") {
+			$title = $client[0];
+		} else {
+			$title = get_the_title();
+		}
+		?>
+		
+			<h2><?php echo $title; ?></h2>
 			<p class="subtitle"><?php echo get_post_meta(get_the_ID(), 'Subtitle', true); ?></p>
 		</div>
 		
