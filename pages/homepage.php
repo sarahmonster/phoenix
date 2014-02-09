6afd60d1dad63d1c07e31dbd94a52562b7d843ae
sarah <sarah@triggersandsparks.com>
2014-02-08 22:38:03 -0500

    Set up homepage.

---
+++ pages/homepage.php
@@ -0,0 +1,14 @@
+<div class="sixcol threepush">
+	<div id="hello">
+		<img alt="Hello! My name is Sarah, and I love design." src="wp-content/themes/new-triggers-sparks/images/i-love-design.png" />
+
+		<p>I make beautiful websites and gorgeous graphics, and I travel around the world as I do it.</p>
+		
+		<p>Want to get to know me better? Check out my <a href="/portfolio">portfolio</a> to see a sampling of some of the work I've done.</p>
+		
+		<p>I also write a <a href="/blog">blog</a>. Sometimes I talk about design, but mostly I just tell stories about my travels.</p>
+		
+		<p>Got a project you want to chat about? <a href="/contact">Drop me a line</a>, and let's make pretty things together!</p>
+		
+	</div>
+</div>
