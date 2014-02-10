f29d8ace91ad32de6685e4cff4a655779312e8f3
sarah <sarah@triggersandsparks.com>
2014-02-09 19:09:19 -0500

    Add sarah photo and list of services to homepage.

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,14 +1,34 @@
-<div class="sixcol threepush">
-	<div id="hello">
-		<img alt="Hello! My name is Sarah, and I love design." src="wp-content/themes/new-triggers-sparks/images/i-love-design.png" />
-
-		<p>I make beautiful websites and gorgeous graphics, and I travel around the world as I do it.</p>
-		
-		<p>Want to get to know me better? Check out my <a href="/portfolio">portfolio</a> to see a sampling of some of the work I've done.</p>
-		
-		<p>I also write a <a href="/blog">blog</a>. Sometimes I talk about design, but mostly I just tell stories about my travels.</p>
-		
-		<p>Got a project you want to chat about? <a href="/contact">Drop me a line</a>, and let's make pretty things together!</p>
-		
+	<div class="threecol onepush">
+			<img alt="Hello! My name is Sarah." src="<?php echo $path; ?>sarah-semark.jpg" class="circular" />
 	</div>
-</div>
+	<div class="sevencol">
+			<img alt="I love design." src="wp-content/themes/new-triggers-sparks/images/i-love-design.png" class="borderless" />
+	</div>
+
+</div><!-- END ROW -->
+<div class="row">
+
+	<div class="sevencol onepush" id="hello">
+			
+			<p>I make beautiful websites and gorgeous graphics, and I travel around the world as I do it.</p>
+			
+			<p>Want to get to know me better? Check out my <a href="/portfolio">portfolio</a> to see a sampling of some of the work I've done.</p>
+			
+			<p>I also write a <a href="/blog">blog</a>. Sometimes I talk about design, but mostly I just tell stories about my travels.</p>
+			
+			<p>Got a project you want to chat about? <a href="/contact">Drop me a line</a>, and let's make pretty things together!</p>
+			
+	</div>
+	
+	<div class="threecol">
+		<h3>What I do</h3>
+		<ul>
+			<li>WordPress websites</li>
+			<li>logo & graphic design</li> 
+			<li>website development</li>
+			<li>UX design</li> 
+			<li>responsive design</li> 
+			<li>identity development</li>
+			<li>make things beautiful</li>
+		</ul>
+	</div>
