76db5179ed28b5e6f60ecb938ad75a845311a0b8
sarah <sarah@triggersandsparks.com>
2014-02-11 00:15:00 -0500

    Set up styling and add icons for service on left side.

--- pages/services.php
+++ pages/services.php
@@ -18,38 +18,90 @@
 
 
 	<div class="sixcol">
-	
-		<h3>Wordpress websites</h3>
-		<p>WordPress is used on over 60 million websites, including almost 20% of the world’s top sites, and with good reason: it's flexible, expandable, and easy-to-use! I build most of my websites using Wordpress. This means you'll be able to update your website as easily as your Facebook status. I customize the site to your particular needs and make sure it's as simple as possible for you to edit and update.</p>
 		
-		<h3>Responsive design</h3>
-		<p>With more and more people now accessing the internet on a mobile device, it is rapidly becoming more vital to offer a site that’s optimized for these devices. A well thought-out responsive design will give you a website that adapts to the size of the device it's being shown on. </p>
+		<div class="service-icon">
+			<img src="<?php echo $path; ?>services/wordpress.png">
+		</div>
+		<div class="service-text">
+			<h3>Wordpress websites</h3>
+			<p>Fast, stable, and easy-to-use, WordPress is probably the perfect platform for your site. I've been building WordPress sites in all shapes and sizes for years, and I know exactly how to tailor it perfectly to your needs.</p>
+		</div>
 		
-		<h3>Domain and hosting</h3> 
-		<p>Don't know what this means? Don't you worry about a thing. I've got it covered. I run my own lightning-fast servers, optimized for WordPress, and keep everything secure and up-to-date for you.</p>
+		<div class="service-icon">
+			<img src="<?php echo $path; ?>services/responsive.png">
+		</div>
+		<div class="service-text">
+			<h3>Responsive design</h3>
+			<p>With more and more people now accessing the internet on a mobile device, it is rapidly becoming more vital to offer a site that’s optimized for these devices. A well thought-out responsive design will give you a website that adapts to the size of the device it's being shown on. </p>
+		</div>
 		
-		<h3>Interface design (UI/UX)</h3>
-		<p>Great interfaces mean that people feel happy using your product. I design beautiful, usable interfaces for web and mobile apps, taking into consideration user needs.</p>
+		<div class="service-icon">
+			<img src="<?php echo $path; ?>services/hosting.png">
+		</div>
+		<div class="service-text">
+			<h3>Domains and hosting</h3> 
+			<p>Don't know what this means? Don't you worry about a thing. I've got it covered. I run my own lightning-fast servers, optimized for WordPress, and keep everything secure and up-to-date for you.</p>
+		</div>
 		
-		<h3>Email newsletters &amp; Social Media Integration</h3> 
-		<p>Want to mass-mail three thousand of your closest friends? I'm well-versed in MailChimp templates, and can create a newsletter template that will degrade well across a variety of email clients. I can also make sure your website interacts well (and matches with) your various social media accounts. </p>
-	
+		<div class="service-icon">
+			<img src="<?php echo $path; ?>services/ux.png">
+		</div>
+		<div class="service-text">
+			<h3>Interface design (UI/UX)</h3>
+			<p>Great interfaces mean that people feel happy using your product. I design beautiful, usable interfaces for web and mobile apps, taking into consideration user needs.</p>
+		</div>
+		
+		<div class="service-icon">
+			<img src="<?php echo $path; ?>services/newsletters.png">
+		</div>
+		<div class="service-text">
+			<h3>Email newsletters &amp; Social Media Integration</h3> 
+			<p>Want to mass-mail three thousand of your closest friends? I'm well-versed in MailChimp templates, and can create a newsletter template that will degrade well across a variety of email clients. I can also make sure your website interacts well (and matches with) your various social media accounts. </p>
+		</div>
 	
 	</div>
 	
 	<div class="sixcol last">
 		
-		<h3>Logos & branding</h3> 
-		<p>First impressions <em>do</em> count. People <em>do</em> judge a book by its cover. The first thing your customers see of you is your logo. What is it telling them? One you've got a logo, you need a business card, letterhead, and maybe a whole range of other materials. I can make sure your look is consistent throughout, so everything just fits right.</p>
+		<div class="service-icon">
+			<img src="<?php echo $path; ?>services/branding.png">
+		</div>
+		<div class="service-text">
+			<h3>Logos &amp; branding</h3> 
+			<p>A great logo should tell the world, with a simple line or shape, what you're all about. Once you've got a logo, you need a business card, letterhead, and maybe a whole range of other materials. I can make sure your look is consistent throughout, so everything just fits right.</p>
+		</div>
 	
-		<h3>Printed promotions</h3> 
-		<p>Brochures, advertisements, mailouts, stickers, t-shirts, flyers, postcards, newsletters, packaging—don't just put your logo on it, make it look lovely to boot. If you can print it out, or show it on a screen, I can make it eye-catching and gorgeous.</p>
-	
-		<h3>Resumes</h3>
-		<p>A beautifully designed and well-written resume will get your foot in the door.</p>
+		<div class="service-icon">
+			<img src="<?php echo $path; ?>services/branding.png">
+		</div>
+		<div class="service-text">
+			<h3>Printed promotions</h3> 
+			<p>Brochures, advertisements, mailouts, stickers, t-shirts, flyers, postcards, newsletters, packaging—don't just put your logo on it, make it look lovely to boot. If you can print it out, or show it on a screen, I can make it eye-catching and gorgeous.</p>
+		</div>
 		
-		<h3>Information design</h3> 
-		<p>Your menus, signs, posters, annual reports, and wedding invitations will have so much more panache if they're professionally designed.</p>
+		<div class="service-icon">
+			<img src="<?php echo $path; ?>services/branding.png">
+		</div>
+		<div class="service-text">
+			<h3>Resumes</h3>
+			<p>A beautifully designed and well-written resume will get your foot in the door.</p>
+		</div>
+		
+		<div class="service-icon">
+			<img src="<?php echo $path; ?>services/branding.png">
+		</div>
+		<div class="service-text">
+			<h3>Book Covers</h3>
+			<p>People <em>do</em> judge a book by its cover. Let's make sure they like it.</p>
+		</div>
+		
+		<div class="service-icon">
+			<img src="<?php echo $path; ?>services/branding.png">
+		</div>
+		<div class="service-text">
+			<h3>Information design</h3> 
+			<p>Your menus, signs, posters, annual reports, and wedding invitations will have so much more panache if they're professionally designed.</p>
+		</div>
 	
 	</div>
 
