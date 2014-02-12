91fc69f600969858830450cdf25ed6464966cacf
sarah <sarah@triggersandsparks.com>
2014-02-11 20:08:25 -0500

    Rearrange and rewrite intro paragraph on services page.

--- pages/services.php
+++ pages/services.php
@@ -1,9 +1,11 @@
 	<div class="fourcol">
 		<img src="<?php echo $path; ?>services/skills.png" class="borderless">
+		
+		<p class="fine-print">Here are some of the things I've made for clients in the past: WordPress blogs, e-commerce sites, custom websites, static websites, Facebook apps, CD covers, posters, menus, labels, logos, stationary, wedding invitations, web apps, thank-you cards, business cards, flyers, postcards, stickers, newspaper &amp; magazine advertisements, museum panel displays, window signage, resumes, social media profile graphics, book covers, iPhone apps, packaging.</p>
 	</div>
 	
 	<div class="eightcol last intro">
-		<p>I've been in business a long time, and that means that there are a <strong>lot</strong> of things I do. Here are some of the things I've made for clients in the past: WordPress blogs, e-commerce sites, custom websites, static websites, Facebook apps, CD covers, posters, menus, labels, logos, stationary, wedding invitations, web apps, thank-you cards, business cards, flyers, postcards, stickers, newspaper &amp; magazine advertisements, museum panel displays, window signage, resumes, social media profile graphics, book covers, iPhone apps, packaging.</p>
+		<p>I've been in business a long time, and that means that there are a <strong>lot</strong> of things I do. I specialize in custom WordPress sites, but there's a whole lot more that I can do. I'm either a very technical designer, or a very aesthetically-inclined developer&mdash;depending on who you ask&mdash;so you can count on me to deliver something as functional as it is beautiful.</p>
 		
 		<p>It might be shorter to give you a list of the things I don't do. Here are a few things I <em>don't</em> do: Flash, SEO, back-end development, waste anyone's time. I've developed strategic partnerships with a whole host of brilliant people. Need an illustration? Copywriting? Search engine optimization? Photography? Custom programming? I know a guy. (Oh, and if you already <em>know</em> a guy, I play well with others.)</p>
 		
