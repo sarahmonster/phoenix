7ad412b1f2e4c545f64ec19764dc88ee1c879168
sarah <sarah@triggersandsparks.com>
2014-02-11 23:24:58 -0500

    Fine-tune services text so that all line lengths are equal.

--- pages/services.php
+++ pages/services.php
@@ -1,7 +1,7 @@
 	<div class="fourcol">
 		<img src="<?php echo $path; ?>services/skills.png" class="borderless">
 		
-		<p class="fine-print">Here are some of the things I've made for clients in the past: WordPress blogs, e-commerce sites, custom websites, static websites, Facebook apps, CD covers, posters, menus, labels, logos, stationary, wedding invitations, web apps, thank-you cards, business cards, flyers, postcards, stickers, newspaper &amp; magazine advertisements, museum panel displays, window signage, resumes, social media profile graphics, book covers, iPhone apps, packaging.</p>
+		<p class="fine-print">Just a few of the things I've made for clients in the past: WordPress blogs, MailChimp templates, e-commerce sites, custom websites, Facebook apps, CD covers, posters, menus, labels, logos, stationary, wedding invitations, web apps, thank-you cards, business cards, flyers, postcards, stickers, newspaper &amp; magazine advertisements, museum panel displays, window signage, resumes, social media profile graphics, book covers, iPhone apps, packaging.</p>
 	</div>
 	
 	<div class="eightcol last intro">
@@ -34,7 +34,7 @@
 		</div>
 		<div class="service-text">
 			<h3>Responsive design</h3>
-			<p>You know how people are always wandering around playing with their phones? Your site needs to be optimized for them. I craft thoughtful, responsive designs that adapt to the size of the device they're displayed on.</p>
+			<p>You know how people are always wandering around playing with their phones? Your site needs to be optimized for them. I craft thoughtful, responsive designs that adapt to any device.</p>
 		</div>
 		
 		<div class="service-icon">
@@ -42,7 +42,7 @@
 		</div>
 		<div class="service-text">
 			<h3>Domains and hosting</h3> 
-			<p>Your website needs a safe place to live. With my <a href="/blog/managed-wordpress-hosting-with-love/">managed hosting service</a>, optimized for WordPress, your site will be stable, secure, and lightning-fast. Personalized support, regular updates, and bulletproof backups come standard.</p>
+			<p>With my <a href="/blog/managed-wordpress-hosting-with-love/">managed hosting service</a>, optimized for WordPress, your site will be stable, secure, and lightning-fast. Personalized support, regular updates, and bulletproof backups come standard.</p>
 		</div>
 		
 		<div class="service-icon">
@@ -50,15 +50,15 @@
 		</div>
 		<div class="service-text">
 			<h3>Interface design (UI/UX)</h3>
-			<p>Great interfaces mean that people feel happy using your product. I design beautiful, usable interfaces for web and mobile apps, taking into consideration user needs.</p>
+			<p>Great interfaces mean that people feel happy using your product. I design beautiful, usable interfaces for web and mobile apps. Happy users keep coming back for more.</p>
 		</div>
 		
 		<div class="service-icon">
 			<img src="<?php echo $path; ?>services/newsletters.png">
 		</div>
 		<div class="service-text">
-			<h3>Email newsletters &amp; Social Media</h3> 
-			<p>Want to mass-mail three thousand of your closest friends? I create newsletter templates that look beautiful across a variety of email clients. While we're at it, I can integrate your social media accounts with your website and other promotions.</p>
+			<h3>Email newsletters</h3> 
+			<p>Want to mass-mail three thousand of your closest friends? I create newsletter templates that look beautiful across a variety of email clients.</p>
 		</div>
 	
 	</div>
@@ -70,7 +70,7 @@
 		</div>
 		<div class="service-text">
 			<h3>Logos &amp; branding</h3> 
-			<p>A great logo speaks volumes with a few simple shapes. Once you've got a logo, you need a business cards, letterhead, and other marketing materials. I can help you build a great brand from the ground up, giving you a cohesive and professional look.</p>
+			<p>A great logo speaks volumes with a few simple shapes. Your business cards, letterhead, and marketing materials reinforce that idea. I can help you build a professional, cohesive brand from the ground up.</p>
 		</div>
 	
 		<div class="service-icon">
@@ -78,7 +78,7 @@
 		</div>
 		<div class="service-text">
 			<h3>Printed promotions</h3> 
-			<p>Brochures, advertisements, mailouts, stickers, t-shirts, flyers, postcards, newsletters, packaging—don't just put your logo on it, make it look lovely to boot. If you can print it out, or show it on a screen, I can make it eye-catching and gorgeous.</p>
+			<p>Brochures, advertisements, stickers, t-shirts, flyers, postcards, packaging&mdash;don&rsquo;t just put your logo on it, make it look lovely to boot. If you can print it, I can make it eye-catching and gorgeous.</p>
 		</div>
 		
 		<div class="service-icon">
@@ -86,7 +86,7 @@
 		</div>
 		<div class="service-text">
 			<h3>Resumes</h3>
-			<p>A beautifully designed and well-written resume will get your foot in the door. I make people gorgeous resumes that scream "hire me!" (My own resume, on the other hand, hasn't been updated since 2009.)</p>
+			<p>A beautifully designed and well-written resume will get your foot in the door. I make people gorgeous resumes that scream &ldquo;hire me!&rdquo; (My own resume, on the other hand, hasn't been updated since 2009.)</p>
 		</div>
 		
 		<div class="service-icon">
@@ -94,7 +94,7 @@
 		</div>
 		<div class="service-text">
 			<h3>Book Covers</h3>
-			<p>People <em>do</em> judge a book by its cover. Let's make sure they like it. Whether you need a print cover or an ebook designed, I can help you create something beautiful and eye-catching.</p>
+			<p>People <em>do</em> judge a book by its cover. Let&rsquo;s make sure they like it. Whether you need a print cover or an ebook designed, I can help you create something beautiful and eye-catching.</p>
 		</div>
 		
 		<div class="service-icon">
@@ -102,7 +102,7 @@
 		</div>
 		<div class="service-text">
 			<h3>Signage &amp; Information design</h3> 
-			<p>Your menus, signs, posters, annual reports, and wedding invitations will have so much more panache if they're professionally designed.</p>
+			<p>Your menus, signs, posters, annual reports, and wedding invitations will have so much more panache if they&rsquo;re professionally designed.</p>
 		</div>
 	
 	</div>
