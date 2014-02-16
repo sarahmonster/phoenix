bb1fdda4f452defb778de9a839a3729af0bd4263
sarah <sarah@triggersandsparks.com>
2014-02-15 20:54:54 -0500

    Set up about and contact pages to use templates.

---
+++ pages/contact.php
@@ -0,0 +1,32 @@
+<div class="sixcol">
+	<p class="cap">Got a question, concern, or just something you think I ought to know? Get in touch with me! I'm based primarily out of Montreal and Ottawa, although I can often be found in far-flung reaches of the globe. If I happen to be in your part of the world at any point, drop me a line and we can meet up in person!</p>
+	
+	<p class="current-location">Sarah is currently in <span>Bogotá, Colombia</span> where it is <span>rainy</span>.</p>
+	
+	<div class="contact-card">
+		<h3>Contact Me</h3>
+		<ul>
+		        <li class="card-email"><a href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#116;&#114;&#105;&#103;&#103;&#101;&#114;&#115;&#097;&#110;&#100;&#115;&#112;&#097;&#114;&#107;&#115;&#046;&#099;&#111;&#109;?subject=hello">hello@</a></li>
+		        <li class="card-twitter"><a href="http://twitter.com/sarahsemark">@sarahsemark</a></li>
+			<li class="card-facebook"><a href="http://www.facebook.com/triggersandsparks">triggersandsparks</a></li>
+			<li class="card-linkedin"><a href="http://ca.linkedin.com/in/triggersandsparks">sarahsemark</a></li>
+		        <li class="card-skype"><script type="text/javascript" src="http://download.skype.com/share/skypebuttons/js/skypeCheck.js"></script><a href="skype:triggersandsparks?call">triggersandsparks</a>
+			</li><li class="card-subscribe"><a href="/feed">subscribe</a></li>
+		</ul>
+	</div>
+
+
+
+	<p class="cap">Note that I am often out of the country. Telephone, carrier pigeon, and showing-up-on-my-doorstep are typically extremely ineffective ways of getting in touch with me. Email, on the other hand, is always with me, even when I'm driving through clifftops in Chile, exploring the markets of Morocco, or trekking through Thailand.</p>
+
+</div>
+
+
+<div class="fivecol onepush last match-col">
+
+	<h3>Request a quote</h3>
+	<p>Want to know what it’s going to cost? Fill in the form below, giving as much detail as you possibly can about your project. Give me 48 hours, and I’ll send you a detailed estimate (or possibly a list of follow-up ques­tions). No strings attached, I swear.</p>
+
+	<?php echo do_shortcode('[contact-form-7 id="1673" title="New Quote Request"]'); ?>
+
+</div>
