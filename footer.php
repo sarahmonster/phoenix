f1467ba66e7bb92ea96f03d7ec460967d8af3ef6
sarah <sarah@triggersandsparks.com>
2015-05-24 03:38:19 +0100

    Add a super-simple footer.

--- footer.php
+++ footer.php
@@ -12,9 +12,10 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'flare' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'flare' ), 'WordPress' ); ?></a>
+			<?php printf( 'Made with &hearts; and', 'flare' ); ?>
+      <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'flare' ) ); ?>"><?php printf( __( '%s', 'flare' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'flare' ), 'Flare', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( __( 'Theme on %1$s', 'flare' ), '<a href="http://github.com/sarahsemark/flare/" rel="designer">Github</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
