a7f069c44c47f30020f9750abf2544e83c53ce53
sarah <sarah@triggersandsparks.com>
2015-06-20 23:46:17 +0100

    Add fontawesome icons and use arrows for navigation.

--- functions.php
+++ functions.php
@@ -105,11 +105,9 @@ add_action( 'widgets_init', 'flare_widgets_init' );
  */
 function flare_scripts() {
 	wp_enqueue_style( 'flare-style', get_stylesheet_uri() );
-
+	wp_enqueue_style( 'flare-icons', get_template_directory_uri() . '/icons/styles.css', array(), '20150620', all );
 	wp_enqueue_script( 'flare-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( 'flare-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
