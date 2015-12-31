e17426399d269972c8b00f3df3bee257cca8f7ba
sarah <sarah@triggersandsparks.com>
2015-12-31 16:07:53 +0000

    Switch from compass to gulp for compiling sass and other stuff.

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+// Include Gulp
+var gulp = require( 'gulp' );
+
+// Include Plugins
+var sass = require( 'gulp-sass' );
+var autoprefixer = require( 'gulp-autoprefixer' );
+var imagemin = require( 'gulp-imagemin' );
+var pngquant = require( 'imagemin-pngquant' );
+var jshint = require( 'gulp-jshint' );
+var concat = require( 'gulp-concat' );
+var cache = require( 'gulp-cache' );
+var sourcemaps = require( 'gulp-sourcemaps' );
+var csscomb = require( 'gulp-csscomb' );
+var livereload = require( 'gulp-livereload' );
+
+// Styles tasks
+gulp.task( 'styles', function() {
+	return gulp.src( 'assets/sass/style.scss' )
+		.pipe( sourcemaps.init() )
+		.pipe( sass( { style: 'expanded' } ).on( 'error', sass.logError ) )
+		.pipe( autoprefixer( { browsers: ['last 2 versions', 'ie >= 9'], cascade: false } ) )
+		.pipe( sourcemaps.write( './' ) )
+		.pipe( csscomb() )
+		.on( 'error', function ( err ) {
+			console.error( 'Error!', err.message );
+		} )
+		.pipe( gulp.dest( './' ) )
+		.pipe( livereload() );
+});
+
+// Scripts
+gulp.task( 'scripts', function() {
+	return gulp.src( 'assets/js/*.js' )
+		.pipe( jshint.reporter( 'default' ) )
+		//.pipe( concat( 'main.js' ) )
+		.pipe( gulp.dest( 'assets/js' ) );
+});
+
+// Images
+gulp.task( 'images', function() {
+	return gulp.src( 'assets/images/*' )
+	.pipe( cache( imagemin( {
+		optimizationLevel: 3,
+		progressive: true,
+		interlaced: true,
+		svgoPlugins: [{ removeViewBox: false }],
+		use: [pngquant()]
+	} ) ) )
+	.pipe( gulp.dest( 'assets/images' ) );
+});
+
+// Watch files for changes
+gulp.task( 'watch', function() {
+	livereload.listen();
+	gulp.watch( 'assets/sass/**/*.scss', ['styles'] );
+	gulp.watch( 'assets/js/**/*.js', ['scripts'] );
+	gulp.watch( 'assets/images/*', ['images'] );
+});
+
+// Default Task
+gulp.task( 'default', ['styles', 'scripts', 'images', 'watch'] );
