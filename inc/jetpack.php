9a56e79faa7de8100ebffd6bfaecdcc863255f6f
sarah <sarah@triggersandsparks.com>
2016-02-26 14:58:04 +0800

    Ensure Infinite Scrolling grabs the right template for the testimonial archive page.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -14,45 +14,49 @@ function phoenix_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container'      => 'main',
 		'footer'         => 'page',
-    'wrapper'        => true,
-    'posts_per_page' => 10,
-    'type'           => 'click',
-    'render'         => 'phoenix_infinite_scroll_render'
+		'wrapper'        => true,
+		'posts_per_page' => 10,
+		'type'           => 'click',
+		'render'         => 'phoenix_infinite_scroll_render'
 	) );
 
-  function phoenix_infinite_scroll_render() {
-    while ( have_posts() ) : the_post();
-      get_template_part( 'template-parts/content', get_post_type() );
-    endwhile;
-  }
+	function phoenix_infinite_scroll_render() {
+		while ( have_posts() ) : the_post();
+			if ( is_post_type_archive( 'jetpack-testimonial' ) ) :
+				get_template_part( 'template-parts/content', 'testimonial' );
+			else :
+				get_template_part( 'template-parts/content', get_post_type() );
+			endif;
+		endwhile;
+	}
 
-  /**
-   * Add theme support for Jetpack responsive videos
-   * See: http://jetpack.me/support/responsive-videos/
-   */
-  add_theme_support( 'jetpack-responsive-videos' );
+	/**
+	 * Add theme support for Jetpack responsive videos
+	 * See: http://jetpack.me/support/responsive-videos/
+	 */
+	add_theme_support( 'jetpack-responsive-videos' );
 
-  /**
-   * Add theme support for Jetpack featured content
-   * See: http://jetpack.me/support/featured-content/
-   */
-  add_theme_support( 'featured-content', array(
-    'filter'    => 'phoenix_get_featured_posts',
-    'max_posts' => 2,
-  ) );
+	/**
+	 * Add theme support for Jetpack featured content
+	 * See: http://jetpack.me/support/featured-content/
+	 */
+	add_theme_support( 'featured-content', array(
+		'filter'    => 'phoenix_get_featured_posts',
+		'max_posts' => 2,
+	) );
 
-  /**
-   * Add theme support for Jetpack testimonials.
-   * See:
-  */
-  add_theme_support( 'jetpack-testimonial' );
+	/**
+	 * Add theme support for Jetpack testimonials.
+	 * See: https://jetpack.me/support/custom-content-types/
+	*/
+	add_theme_support( 'jetpack-testimonial' );
 
-  /**
-   * Add theme support for Jetpack site logo
-   * See: http://jetpack.me/support/site-logo/
-   */
-  add_image_size( 'phoenix-logo', 700 ); // Restrict logo to 700 pixels in width (double-sized for hi-res devices)
-  add_theme_support( 'site-logo', array( 'size' => 'phoenix-logo' ) );
+	/**
+	 * Add theme support for Jetpack site logo
+	 * See: http://jetpack.me/support/site-logo/
+	 */
+	add_image_size( 'phoenix-logo', 700 ); // Restrict logo to 700 pixels in width (double-sized for hi-res devices)
+	add_theme_support( 'site-logo', array( 'size' => 'phoenix-logo' ) );
 
 }
 add_action( 'after_setup_theme', 'phoenix_jetpack_setup' );
