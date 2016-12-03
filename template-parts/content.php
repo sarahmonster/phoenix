1fea840373502d807f3051acf882309abb0693e6
sarah <sarah@triggersandsparks.com>
2016-12-03 19:33:36 +0000

    Use skewy styles for blog archive pages.

--- template-parts/content.php
+++ template-parts/content.php
@@ -5,26 +5,24 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header">
+
 		<?php
 		// show the featured image, if there's one set
 		printf( '<a href="%1$s" rel="bookmark" title="%2$s">',
 						esc_url( get_permalink() ),
 						esc_html( phoenix_subtitle() )
 					);
-		if ( has_post_thumbnail() ) {
-			the_post_thumbnail();
-		} else {
+		if ( has_post_thumbnail() ) :
+			$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
+			<div class="phoenix-panel-background" style="background-image:url(<?php echo esc_url( $thumbnail[0] ); ?>)"></div>
+		<?php
+		else :
 			echo '<img src="/wp-content/themes/phoenix/images/placeholder.png" alt="View post" />';
-		}
+		endif;
 		echo '</a>';
 		?>
-	<?php phoenix_post_category(); ?>
-	<header class="entry-header">
-		<?php if ( 'post' == get_post_type() ) : ?>
-			<div class="entry-meta">
-				<?php phoenix_post_date(); ?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
 
 		<?php
 		printf( '<h2 class="entry-title"><a href="%1$s" rel="bookmark">%2$s</a></h2>',
@@ -35,6 +33,15 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+
+		<?php phoenix_post_category(); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php phoenix_post_date(); ?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+
 		<?php
 			/* translators: %s: Name of current post */
 			the_excerpt();
