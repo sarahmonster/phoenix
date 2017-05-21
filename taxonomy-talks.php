166b22907c65747f308e9e075ad8ee8e85312319
sarah <sarah@triggersandsparks.com>
2017-05-21 16:46:04 +0100

    Add a start taxonomy template and content for style guides page.

---
+++ taxonomy-talks.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying talk archive pages.
+ * Uses some special sauce.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Phoenix
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<div class="entry-content">
+				<?php get_template_part( 'template-parts/talk', 'styleguides'  ); ?>
+			</div>
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_type() );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php phoenix_numeric_pagination(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
---
+++ template-parts/talk-styleguides.php
@@ -0,0 +1,25 @@
+<p>The line between design and development is getting blurrier, and our approach to web design is becoming more modular. How do we keep track of it all?</p>
+
+<p><em>Style guides to the rescue!</em></p>
+
+<p>Using style guides that update as our projects progress, we can visually document our work and ensure that everything stays consistent. They may seem daunting and time-consuming at first, but they don’t have to be.</p>
+
+<p>How do I get started with living style guides? What’s a living style guide anyway, and how do I know if mine is dead? Isn’t that going to slow me down? Why should I bother?</p>
+
+<p>Let’s find out.</p>
+
+
+<h3>More about style guides</h3>
+<ul>
+	<li><a href="http://styleguides.io">Styleguides.io</a> is basically the bible of style guides.</li>
+</ul>
+
+<h3>Style guide examples</h3>
+<ul>
+	<li><a href="https://woocommerce.com/style-guide/">WooCommerce</a></li>
+</ul>
+
+<h3>Tools to build a style guide</h3>
+<ul>
+	<li><a href="http://styleguide.sc5.io/">SC5 Style guide generator</a></li>
+</ul>
