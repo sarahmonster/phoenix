99b2945d653ab56a73569ba2ec08561b89a6de8d
sarah <sarah@triggersandsparks.com>
2017-05-29 07:23:05 +0100

    Add content for svg icons talk.

---
+++ template-parts/talk-svg-icons.php
@@ -0,0 +1,22 @@
+<p>Once upon a time, icon fonts were the exciting new tools we used to build better websites. That time has passed, yet most of us are still relying on a tool that’s no longer relevant in the landscape of the modern web. In this talk, I’ll discuss the various merits of making the switch to SVG and some neat things you can do with SVG. We’ll touch on various use cases, and present some ideas for determining the workflow that’s best suited to your individual needs. We’ll provide some practical tips and tools for using SVG sprites more easily and efficiently in your work. We’ll also discuss ideas for implementing SVG within WordPress themes and plugins in a simple, consistent manner. Along the way, we’ll discuss various challenges you may encounter, and how to approach them. By the time we’re done, you’ll be fully armed to start using SVG today!</p>
+
+<p><a href="http://github.com/sarahsemark/easy-as-svg">Easy-as-SVG Github repo</a>. (Code sources)<p>
+
+<h3>Resources</h3>
+
+<ul>
+    <li><a href="https://css-tricks.com/using-svg/">Basics of SVG</a></li>
+    <li><a href="https://24ways.org/2014/an-overview-of-svg-sprite-creation-techniques/">An overview of SVG sprite creation methods</a></li>
+    <li><a href="http://bdatech.org/what-technology/typefaces-for-dyslexia/">Typefaces for dyslexia</a></li>
+    <li><a href="https://css-tricks.com/lodge/svg/">CSS Tricks' vault of SVG articles</a></li>
+    <li><a href="https://www.youtube.com/watch?v=9xXBYcWgCHA">Seren Davies: Death to icon fonts</a></li>
+    <li><a href="https://css-tricks.com/creating-svg-icon-system-react/">Building an icon system with React</a></li>
+    <li><a href="https://www.smashingmagazine.com/2014/11/styling-and-animating-svgs-with-css/">Styling and animating SVGs with CSS</a></li>
+    <li><a href="https://css-tricks.com/a-complete-guide-to-svg-fallbacks/">A guide to SVG fallbacks</a></li>
+    <li><a href="https://sarasoueidan.com/blog/svg-coordinate-systems/">Understanding the SVG coordinate system</a></li>
+    <li><a href="https://css-tricks.com/accessible-svgs/">Accessible SVGs</a></li>
+    <li><a href="https://css-tricks.com/a-complete-guide-to-svg-fallbacks/">A complete guide to SVG fallbacks</a></li>
+    <li><a href="https://www.theguardian.com/artanddesign/2016/sep/06/emojis-shigetaka-kurita-mark-davis-coding-language">Emoji, the world's fastest-growing language</a></li>
+    <li><a href="http://tympanus.net/codrops/2015/07/16/styling-svg-use-content-css/">Styling SVG &lt;use&gt; content with CSS</a></li>
+    <li><a href="https://svgontheweb.com/">SVG on the web: a practical guide</a></li>
+</ul>
