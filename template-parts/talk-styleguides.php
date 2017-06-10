c8e47e80b261b7b19bb363664caa924bc58c36a3
sarah <sarah@triggersandsparks.com>
2017-06-10 23:43:34 +0100

    Add some new resources to style guide talk page.

--- template-parts/talk-styleguides.php
+++ template-parts/talk-styleguides.php
@@ -12,12 +12,15 @@
 <h3>More about style guides</h3>
 <ul>
 	<li><a href="http://styleguides.io">Styleguides.io</a> is basically the bible of style guides.</li>
+	<li><a href="https://www.alleyinteractive.com/news/advanced-techniques-for-the-sc5-styleguide-generator/">Advanced techniques for SC5</a></li>
+	<li><a href="http://varya.me/en/posts/sc5-style-guide-next-level/">Living SC5 style guides, the next level</a></li>
+	<li><a href="https://sc5.io/posts/visual-regression-testing/">Visual regression testing</a></li>
 </ul>
 
 <h3>Style guide examples</h3>
 <ul>
 	<li><a href="https://woocommerce.com/style-guide/">WooCommerce</a></li>
-	<li><a href="https://lucijanblagonic.github.io/wordcamp-style-guide/styleguide/">WCEU</a></li>
+	<li><a href="https://lucijanblagonic.github.io/wordcamp-style-guide/styleguide/">WCEU</a> (uses KSS)</li>
 	<li><a href="http://sfdc-styleguide.herokuapp.com/">Salesforce</a></li>
 
 </ul>
@@ -25,7 +28,11 @@
 <h3>Tools to build a style guide</h3>
 <ul>
 	<li><a href="https://frontify.com/styleguide">Frontify styleguide</a></li>
-	<li><a href="https://github.com/adactio/Pattern-Primer">Super-simple pattern primer</a></li>
-	<li><a href="http://warpspire.com/kss/">Knyle Style Sheets</a></li>
+	<li><a href="https://github.com/adactio/Pattern-Primer">Pattern primer</a></li>
+	<li><a href="http://warpspire.com/kss/">Knyle Style Sheets</a> (KSS)</li>
 	<li><a href="http://styleguide.sc5.io/">SC5 Style guide generator</a></li>
+	<li><a href="http://fbrctr.github.io">Fabricator</a></li>
+	<li><a href="https://github.com/nopr/sassdown">Sassdown</a></li>
+	<li><a href="https://github.com/livingstyleguide/livingstyleguide">Living Style Guide</a></li>
+	<li><a href="http://bradfrost.github.io/style-guide-guide">Style guide guide</a></li>
 </ul>
