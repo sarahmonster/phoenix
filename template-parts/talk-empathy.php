68633d5d73b32f6c55f31737eefde1bf6140de9e
sarah <sarah@triggersandsparks.com>
2017-09-01 10:45:31 +0200

    Add a page for empathy talk.:

---
+++ template-parts/talk-empathy.php
@@ -0,0 +1,42 @@
+<p>If you want to build websites that people love, you need to understand those people. But what happens if you're just not a people person?</p>
+
+<p>Don’t worry: empathy is a skill that anyone can learn. It just takes a little practise. Approaching design from an inclusive perspective has lead to some of the most innovative designs we use every day, while a lack of empathy can lead to catastrophic failures. Let's learn how to see diversity and disability as opportunities—not obstacles.</p>
+
+<p>By putting ourselves in our users’ shoes, we can make the web a better place for everyone.</p>
+
+<h3>Further reading</h3>
+<ul>
+	<li><a href="http://leecrutchley.co.uk/blog/neural-wifi">Neural wifi</a></li>
+	<li><a href="http://mentalfloss.com/article/64654/why-is-yawning-contagious">Why is yawning contagious?</a></li>
+	<li><a href="http://universaldesign.ie/What-is-Universal-Design/The-7-Principles/">The seven principles of Universal Design</a></li>
+	<li><a href="https://ssir.org/articles/entry/the_curb_cut_effect">The curb-cut effect</a></li>
+
+	<li><a href="//projectempathyvr.com">Project empathy</a></li>
+	<li><a href="http://nautil.us/issue/51/limits/no-you-cant-feel-sorry-for-everyone-rp">No, you can't feel sorry for everyone</a></li>
+	<li><a href="http://www.independent.co.uk/life-style/health-and-families/features/why-a-lack-of-empathy-is-the-root-of-all-evil-6279239.html">Why a lack of empathy is the root of all evil</a></li>
+
+	<li><a href="https://interactive.aljazeera.com/aje/shorts/india-menstruation-man/">India's menstruation man</a></li>
+	<li><a href="http://www.yesmagazine.org/happiness/empathy-heroes-st-francis-john-howard-griffin-patricia-moore">Empathy heros</a></li>
+	<li><a href="http://www.greatproductsbydesign.com/">Well Designed: How to use empathy to create products people love</a></li>
+	<li><a href="http://rosenfeldmedia.com/books/build-better-products/">Build Better Products: A Modern Approach to Building Successful User-Centered Products</a></li>
+</ul>
+
+<h3>Research</h3>
+<ul>
+	<li><a href="https://www.psychologytoday.com/blog/the-athletes-way/201402/do-mirror-neurons-help-create-social-understanding">Do mirror neurons help create social understanding?</a></li>
+	<li><a href="http://ns.umich.edu/new/releases/7724-empathy-college-students-don-t-have-as-much-as-they-used-to">Empathy: College students don't have as much as they used to</a></li>
+	<li><a href="http://tiie.w3.uvm.edu/blog/can-virtual-reality-teach-empathy/">Can virtual reality teach empathy?</a></li>
+	<li><a href="http://www.3playmedia.com/2015/08/28/who-uses-closed-captions-not-just-the-deaf-or-hard-of-hearing/">Who uses closed captions?</a></li>
+	<li><a href="http://www.nytimes.com/2009/04/07/health/07pati.html?mcubz=3">Radiologist adds a human touch: photos </a></li>
+	<li><a href="https://www.statnews.com/2016/07/19/reading-boosts-empathy/">Reading boosts empathy</a></li>
+	<li><a href="http://onlinelibrary.wiley.com/doi/10.1111/jasp.12279/full">The greatest magic of Harry Potter: Reducing prejudice</a></li>
+</ul>
+
+<h3>Tests &amp; tools</h3>
+<ul>
+	<li><a href="//empathyprompts.com">Empathy prompts</a></li>
+	<li><a href="http://socialintelligence.labinthewild.org/mite/">Reading the eyes in the mind test</a></li>
+	<li><a href="https://psychology-tools.com/empathy-quotient/">The empathy quotient test</a></li>
+	<li><a href="https://www.theguardian.com/world/ng-interactive/2016/apr/27/6x9-a-virtual-experience-of-solitary-confinement">6x9 A virtual experience of solitary confinement</a></li>
+	<li><a href="https://medium.com/the-xplane-collection/updated-empathy-map-canvas-46df22df3c8a">Empathy maps</a></li>
+</ul>
