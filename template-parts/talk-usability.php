006deac591ff1f613a0bf3e4b8978a9ffac99314
sarah <sarah@triggersandsparks.com>
2018-07-30 19:37:51 +0100

    Add usability talk.

---
+++ template-parts/talk-usability.php
@@ -0,0 +1,30 @@
+<p>It doesn't matter how brilliant your code is or how beautiful your design is—if your visitors can't use your website, it's useless. A website that's designed with your users in mind is the best way to achieve your goals—but how do we go about building websites that users love?</p>
+
+<p>In this talk, we'll take a look at the importance of website usability and we'll offer up some practical advice for ways to improve your own site. We'll also give an introduction to usability testing, along with ideas for low-friction implementation. Along the way, we'll look at lots of examples of websites that get it wrong, and a few that do it really well.</p>
+
+<p>You'll leave with lots of ideas to help make your websites more effective.</p>
+
+<h3>More information</h3>
+<ul>
+	<li><a href="https://www.linkedin.com/pulse/300-million-button-jared-spool/">The $300 million button</a></li>
+	<li><a href="https://www.nngroup.com/articles/usability-101-introduction-to-usability/">Usability 101: An introduction to usability</a></li>
+	<li><a href="http://www.cracked.com/blog/the-5-worst-error-messages-in-history-technology/">The 5 worst error messages in history</a></li>
+	<li><a href="https://conversionxl.com/blog/error-messages/">4 common mistakes with error messages</a></li>
+</ul>
+
+
+<h3>Recommended reading</h3>
+<ul>
+	<li><a href="http://rosenfeldmedia.com/books/build-better-products/">Build Better Products: A Modern Approach to Building Successful User-Centered Products</a></li>
+</ul>
+
+<h3>Better content</h3>
+
+<ul>
+	<li><a href="https://www.onthedotcreations.com/2014/06/create-scale-in-your-product-photography.html">Create scale in your product photography</a></li>
+	<li><a href="https://www.bigcommerce.com/blog/how-to-rock-product-photography-on-a-budget/">How to master product photography on a budget</a></li>
+
+	<li><a href="https://www.nngroup.com/articles/how-users-read-on-the-web/">How users read on the web</a></li>
+
+	<li><a href="https://www.thoughtco.com/solutions-to-protect-web-forms-from-spam-3467469">Modern solutions to protect web forms from spam</a></li>
+</ul>
